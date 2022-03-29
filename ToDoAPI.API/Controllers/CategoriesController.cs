9641709f78bd4197e8564b33060d2c51c14f385e
jazminerizo <97970995+jazminerizo@users.noreply.github.com>
2022-03-29 18:55:59 -0500

    initial commit

---
+++ ToDoAPI.API/Controllers/CategoriesController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ToDoAPI.API.Models;
+using ToDoAPI.DATA.EF;
+using System.Web.Http.Cors;
+
+namespace ToDoAPI.API.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class CategoriesController : ApiController
+    {
+        ToDoEntities db = new ToDoEntities();
+
+        public IHttpActionResult GetCategories()
+        {
+            List<CategoryViewModel> cats = db.Categories.Select(c => new CategoryViewModel()
+            {
+                CategoryId = c.CategoryId,
+                Name = c.Name,
+                Description = c.Description
+            }).ToList<CategoryViewModel>();
+
+            if (cats.Count == 0)
+                return NotFound();
+
+            return Ok(cats);
+            
+        }//end of GetCategories
+
+        public IHttpActionResult GetCategory(int id)
+        {
+            CategoryViewModel cat = db.Categories.Where(c => c.CategoryId == id).Select(c => new CategoryViewModel()
+            {
+                CategoryId = c.CategoryId,
+                Name = c.Name,
+                Description = c.Description
+            }).FirstOrDefault();
+
+            if (cat == null)
+            {
+                return NotFound();
+            }
+            return Ok();
+
+
+
+        }
+
+
+        public IHttpActionResult PostCategory(CategoryViewModel cat)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid Data");
+            }
+
+            db.Categories.Add(new Category()
+            {
+                Name = cat.Name,
+                Description = cat.Description
+            });
+
+            db.SaveChanges();
+            return Ok();
+        }// end of PostCategory
+
+        //POST
+        public IHttpActionResult PutCategory(CategoryViewModel cat)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid Data");
+            }
+
+            Category existingCat = db.Categories.Where(c => c.CategoryId == cat.CategoryId).FirstOrDefault();
+
+            if (existingCat != null)
+            {
+                existingCat.Name = cat.Name;
+                existingCat.Description = cat.Description;
+                db.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }//end put
+
+        public IHttpActionResult DeleteCategory(int id)
+        {
+            Category cat = db.Categories.Where(c => c.CategoryId == id).FirstOrDefault();
+
+            if (cat != null)
+            {
+                db.Categories.Remove(cat);
+                db.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }//end delete
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+
+        }
+    }
+}
---
+++ ToDoAPI.API/Controllers/ToDoController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ToDoAPI.API.Models;
+using ToDoAPI.DATA.EF;
+using System.Web.Http.Cors;
+
+namespace ToDoAPI.API.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class ToDoController : ApiController
+    {
+        ToDoEntities db = new ToDoEntities();
+        //GET - /api/todo
+        public IHttpActionResult GetToDo()
+        {
+            List<ToDoItemsViewModel> todos = db.TodoItems.Include("Category").Select(t => new ToDoItemsViewModel()
+            {
+                TodoId = t.TodoId,
+                Action = t.Action,
+                Done = t.Done,
+                CategoryId = t.CategoryId,
+                Category = new CategoryViewModel()
+                {
+                    CategoryId = t.Category.CategoryId,
+                    Name = t.Category.Name,
+                    Description = t.Category.Description
+                }
+
+            }).ToList<ToDoItemsViewModel>();
+
+            if (todos.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(todos);
+        }//end of GetToDo()
+
+        //GET api/todo/id
+        public IHttpActionResult GetTodo(int id)
+        {
+            ToDoItemsViewModel todo = db.TodoItems.Include("Category").Where(t => t.TodoId == id).Select(t => new ToDoItemsViewModel()
+            {
+                TodoId = t.TodoId,
+                Action = t.Action,
+                Done = t.Done,
+                CategoryId = t.CategoryId,
+                Category = new CategoryViewModel()
+                {
+                    CategoryId = t.Category.CategoryId,
+                    Name = t.Category.Name,
+                    Description = t.Category.Description
+                }
+            }).FirstOrDefault();
+
+            if (todo == null)
+                return NotFound();
+
+            return Ok(todo);
+           
+        }// end GetTo(id)
+
+        //POST - api/todo (HttpPost)
+        public IHttpActionResult PostTodo(ToDoItemsViewModel todo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid Data");
+            }//end of if
+
+            TodoItem newTodoItem = new TodoItem()
+            {
+                TodoId = todo.TodoId,
+                Action = todo.Action,
+                Done = todo.Done,
+                CategoryId = todo.CategoryId
+            };
+
+            db.TodoItems.Add(newTodoItem);
+            db.SaveChanges();
+
+            return Ok(newTodoItem);
+
+            
+
+
+        }//end PostTodo()
+
+
+        public IHttpActionResult PutResource(ToDoItemsViewModel todo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid Data");
+            }
+
+            //Fet the resource from the db so we can modify it
+            TodoItem existingTodo = db.TodoItems.Where(t => t.TodoId == todo.TodoId).FirstOrDefault();
+
+            //modify the resource
+            if (existingTodo != null)
+            {
+                existingTodo.TodoId = todo.TodoId;
+                existingTodo.Action = todo.Action;
+                existingTodo.Done = todo.Done;
+                existingTodo.CategoryId = todo.CategoryId;
+                
+                //
+                db.SaveChanges();
+                return Ok();
+            }
+
+            else
+            {
+                return NotFound();
+            }
+
+
+        }//end of put
+         //DELETE - api/Resources/id (HTTPDelete)
+        public IHttpActionResult DeleteTodo(int id)
+        {
+            TodoItem todo = db.TodoItems.Where(r => r.TodoId == id).FirstOrDefault();
+
+            if (todo != null)
+            {
+                db.TodoItems.Remove(todo);
+                db.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+
+
+    }// end of class
+}// end of namespace
